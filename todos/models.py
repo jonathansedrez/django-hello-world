b1607e7a81a88784457787bcc129349a3eaa687a
Jonathan Sedrez <jonathansedrez@gmail.com>
2025-06-20 10:12:03 -0300

    feat: add todos database

--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Todo(models.Model):
+  todo_label = models.CharField(max_length=200)
+  is_checked = models.BooleanField(default=False)
