25c605e20f86a68d3c21c5b359ba355646e7bc7a
Jonathan Sedrez <jonathansedrez@gmail.com>
2025-06-23 10:18:45 -0300

    feat: add view to list todos

--- todos/views.py
+++ todos/views.py
@@ -1,6 +1,17 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.template import loader
+
+from .models import Todo
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    todos_list = Todo.objects.all()
+    template = loader.get_template("todos/index.html")
+    context = { "todos_list": todos_list }
+    return HttpResponse(template.render(context, request))
+
+# OR
+# def index(request):
+#     todos_list = Todos.objects.all()
+#     context = { "todos_list": todos_list }
+#     return render(request, "todos/index.html", context)
