18607b002398e01bf713fb42ce21a05e10a89176
Jonathan Sedrez <jonathansedrez@gmail.com>
2025-07-01 10:10:55 -0300

    feat: add todos details page

--- todos/urls.py
+++ todos/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<int:todo_id>/", views.details, name="details"),
 ]
--- todos/views.py
+++ todos/views.py
@@ -1,9 +1,9 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.shortcuts import render
 from django.template import loader
 
 from .models import Todo
 
-# Create your views here.
 def index(request):
     todos_list = Todo.objects.all()
     template = loader.get_template("todos/index.html")
@@ -15,3 +15,10 @@ def index(request):
 #     todos_list = Todos.objects.all()
 #     context = { "todos_list": todos_list }
 #     return render(request, "todos/index.html", context)
+
+def details(request, todo_id):  
+      try:
+           todo = Todo.objects.get(pk=todo_id)
+      except Todo.DoesNotExist:
+          raise Http404("todo does not exist")      
+      return render(request, "todos/details.html", {todo: todo})
