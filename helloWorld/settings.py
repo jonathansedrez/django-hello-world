1c493b8f3bcb80f47443a3d24cdea28d013e50f1
Jonathan Sedrez <jonathansedrez@gmail.com>
2025-07-17 10:23:43 -0300

    feat: add scss processors and compilers

--- helloWorld/settings.py
+++ helloWorld/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_libsass',
+    'compressor'
 ]
 
 MIDDLEWARE = [
@@ -116,8 +118,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+COMPRESS_PRECOMPILERS = (
+    ('text/x-scss', 'django_libsass.SassCompiler'),
+) # libsass compiler
+
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'compressor.finders.CompressorFinder',
+] # Needed fot compressor static files
